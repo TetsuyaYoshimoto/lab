7c872f11986aa4c9941a9cd64b2d9394a77c1d90
yt <xedvc3.phone.8@gmail.com>
2017-06-27 15:51:14 +0900

    Update answord2vec.py

--- answord2vec.py
+++ answord2vec.py
@@ -5,38 +5,16 @@ from gensim.models import word2vec
 import sys
 
 def main():
-    model = word2vec.Word2Vec.load("out.model")
-    while(True):
-        try:
-            posi = input("Please Positive word : ").split()
-            if len(posi) != 0:
-                break 
-            print("Please Retry! ")
-        except UnicodeDecodeError:
-            print("Please Retry! ")
-
-    while(True):
-        try:
-            nega = input("Please Negative word : ").split()
-            break
-        except UnicodeDecodeError:
-            print("Please Retry! ")
-    while(True):
-        try:
-            num = int(input("Please Top Number : "))
-            break
-        except ValueError:
-            print("Please Retry! ")
-
-
-    if len(posi) == 0: posi = ""
-    if len(nega) == 0: nega = ""
-    results = model.most_similar(positive=posi, negative=nega, topn=num)
-
-    for result in results:
-        print(result[0], "\t", result[1])
+    for _ in range(4):
+        word = input()
+        print(word)
+        model = word2vec.Word2Vec.load("../out.model")
+        results = model.most_similar(positive=word, topn=int(10))
+        
+        for result in results:
+            print(result[0], "\t", result[1])
+        print("==================================")
 
 if __name__=="__main__":
     main()
-
 
