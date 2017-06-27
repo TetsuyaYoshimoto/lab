9032ec7513788f647e25a7363b595f7086eb4b37
yt <xedvc3.phone.8@gmail.com>
2017-06-27 16:20:33 +0900

    Update answord2vec.py

--- answord2vec.py
+++ answord2vec.py
@@ -5,16 +5,14 @@ from gensim.models import word2vec
 import sys
 
 def main():
-    for _ in range(4):
-        word = input()
-        #Your put on model Path
-        model = word2vec.Word2Vec.load("../out.model")
-        #Output similer word top 10
-        results = model.most_similar(positive=word, topn=int(10))
+    word = input()
+    #Your put on model Path
+    model = word2vec.Word2Vec.load("../out.model")
+    #Output similer word top 10
+    results = model.most_similar(positive=word, topn=int(10))
         
-        for result in results:
-            print(result[0], "\t", result[1])
-        print("==================================")
+    for result in results:
+        print(result[0], "\t", result[1])
 
 if __name__=="__main__":
     main()
