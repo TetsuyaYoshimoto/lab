aca774db8385459f609c5c08d8884b04f65114d0
yt <xedvc3.phone.8@gmail.com>
2017-06-28 11:27:57 +0900

    Update answord2vec.py

--- answord2vec.py
+++ answord2vec.py
@@ -5,7 +5,7 @@ from gensim.models import word2vec
 import sys
 
 def main():
-    word = input()
+    word = input(Please Wrod : ")
     #Your put on model Path
     model = word2vec.Word2Vec.load("../out.model")
     #Output similer word top 10
